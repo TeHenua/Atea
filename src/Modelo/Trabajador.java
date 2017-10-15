b748dac380359a9fd959b789178bba7e1219a350
Unknown <sirlocus66@gmail.com>
2017-10-15 22:27:15 +0200

    Clase trabajador

---
+++ src/Modelo/Trabajador.java
@@ -0,0 +1,20 @@
+package Modelo;
+
+public class Trabajador {
+
+    private String dni;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String fechaNac;
+    private String lugarNac;
+    private String direccion;
+    private String localidad;
+    private int codigoPos;
+    private int fijo;
+    private int movil;
+    private String email;
+
+    private user user;
+
+}
