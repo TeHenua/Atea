670a896327b26d0c11f7c7b0efc3a3891e20bd32
Unknown <sirlocus66@gmail.com>
2017-10-15 21:09:22 +0200

    declaracion variables

--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -2,4 +2,27 @@ package Modelo;
 
 public class Usuario {
     private String nombre;
+    private String dni;
+    private String apellido1;
+    private String apellido2;
+    private String fecha_nac;
+    private String direccion;
+    private String localidad;
+    private String codigo_pos;
+    private String provincia;
+    private String colegio;
+    private String ocupacion;
+    private String diagnostico;
+    private String grado_discapacidad;
+    private String grado_dependencia;
+    private String puntos_movilidad;
+    private String demanda;
+    private String num_ss;
+    private String tis;
+    private String primera_entrevista;
+    private int socio_id;
+    private String alerta_custodia;
+    private String alerta_medica;
+    private String nombreSocio;
+    private String estado;
 }
