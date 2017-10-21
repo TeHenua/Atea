b8a37148e9f884a78a3f38d54c7352f4b85671a4
Unknown <tehenua@gmail.com>
2017-10-21 19:45:57 +0200

    Clases Usuario,Socio y ControladorBaseDatos

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/Vista/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Vista/Socio.fxml"));
         primaryStage.setTitle("aTEA - Iniciar sesión");
-        primaryStage.setScene(new Scene(rootr));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -1,15 +1,16 @@
 package Modelo;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Usuario {
 
-    private int id;
     private String nombre;
     private String dni;
     private String apellido1;
     private String apellido2;
-    private Date fechaNacimiento;
+    private Integer numSocio;
+    private java.sql.Date fechaNac;
     private String direccion;
     private String localidad;
     private int codigoPos;
@@ -26,11 +27,31 @@ public class Usuario {
     private String primeraEntrevista;
     private String alertaCustodia;
     private String alertaMedica;
-    private String nombreSocio;
     private String estado;
-
+    //relaciones
     private ArrayList<Socio> socios;
     private Grupo grupo;
     private ArrayList<Consulta> consultas;
     private ArrayList<Contacto> contactos;
+
+    public boolean guardarUsuario(){//aqui se guarda el usuario en la base de datos IMPORTANTE QUITAR ID AUTOINCREMENTAL
+
+        ControladorBaseDatos.conectar();
+
+        try {//FALTA EL SOCIO ID
+            PreparedStatement ps = ControladorBaseDatos.getConexion().prepareStatement("INSERT INTO USUARIOS(" +
+                    "ID,NOMBRE,APELLIDO1,APELLIDO2,NUM_SOCIO,FECHA_NAC,LUGAR_NAC,DIRECCION,LOCALIDAD,PROVINCIA,CODIGO_POS," +
+                    "COLEGIO,OCUPACION,DIAGNOSTICO)" +
+                    " VALUES((SELECT NVL(MAX(ID),0)+1 FROM USUARIOS),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?");
+            ps.setString(1,nombre);
+            ps.setString(2,apellido1);
+            ps.setString(3,apellido2);
+            ps.setInt(4,numSocio);
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
